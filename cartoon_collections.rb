87be9eca45fe6201828e8118131418b21a746da7
paulleerentveld <paul@veldnet.com.au>
2022-06-16 17:44:12 +1000

    lab done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
+
+end
+
+def summon_captain_planet(array)
+  array.map do |word|
+    word.capitalize + "!"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(array)
+  array.any?{|word| word.length > 4}
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types.detect(array)
+  array.detect{|snack| cheese_types.include?(snack)}
+  
 end
+
+
